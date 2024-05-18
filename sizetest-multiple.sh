64e131f5c8343cc5f74536a3c03055d33837b0bb
Franco Perez <56205122+dev-lang@users.noreply.github.com>
2024-05-18 10:10:14 -0300

    Update sizetest-multiple.sh
    función handle_error para interceptar errores de conexión

--- sizetest-multiple.sh
+++ sizetest-multiple.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
 # Función para obtener el tamaño del archivo en el servidor
+
+handle_error() {
+    http_code=$1
+    echo "Error: $( [ "$http_code" -eq 404 ] && echo "Archivo no encontrado" || echo "HTTP $http_code" )"
+}
+
 get_remote_size() {
     url=$1
-    remote_size=$(curl --socks5-hostname localhost:9050 -sI "$url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-    echo $remote_size
+    response=$(curl --socks5-hostname localhost:9050 -sI -w "%{http_code} %{size_download}\n" -o /dev/null "$url")
+    http_code=$(echo "$response" | awk '{print $1}')
+    size=$(echo "$response" | awk '{print $2}')
+
+    if [ "$http_code" -eq 200 ]; then
+        echo "$size"
+    elif [ "$http_code" -eq 000 ]; then
+        echo "Error: No se pudo conectar al servidor"
+    else
+        handle_error "$http_code"
+    fi
 }
 
 url_list="urls.txt"
 
 while IFS= read -r url; do
     remote_size=$(get_remote_size "$url")
-    echo "El tamaño del archivo en el servidor para $url es: $remote_size bytes"
+
+    if [[ "$remote_size" == "Error: "* ]]; then
+        echo "No se pudo obtener el tamaño del archivo para $url $remote_size"
+    else
+        echo "El tamaño del archivo en el servidor para $url es: $remote_size bytes"
+    fi
 done < "$url_list"
